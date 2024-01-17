cd4168f0875a6ba10ac8b09da69baf1ccba20fc2
veerender <rathod.veerender@ibm.com>
2024-01-17 19:29:10 +0530

    Open weather report using servlet,ajax

---
+++ my-wknd-site-guide/ui.apps/src/main/content/jcr_root/apps/my-wknd-site/components/weatherforecast/weatherclientlib/js/weatherforecast.js
@@ -0,0 +1,17 @@
+
+$(document).ready(function() {
+    console.log("ready");
+    // This WILL work because we are listening on the 'document', 
+    // for a click on an element with an ID of #test-element
+    $(document).on("click","#test-element",function() {
+        alert("click bound to document listening for #test-element");
+    });
+
+    // This will NOT work because there is no '#test-element' ... yet
+    $("#test-element").on("click",function() {
+        alert("click bound directly to #test-element");
+    });
+
+    // Create the dynamic element '#test-element'
+    $('body').append('<div id="test-element">Click mee</div>');
+});
